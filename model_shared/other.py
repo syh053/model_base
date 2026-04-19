bbd7df952ed9e41f2bbc0bb8da92d7593a3c56e7
syh053 <syh053@gmail.com>
2026-04-19 11:51:05 +0800

    feat(main): 建立 Model 基本工具

---
+++ model_shared/other.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import UUID, text, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class ModelBase(DeclarativeBase):
+    id: Mapped[UUID] = mapped_column(UUID,
+                                     primary_key=True,
+                                     server_default=text("gen_random_uuid()"),
+                                     comment="id")
+
+class TimeMixin(DeclarativeBase):
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), comment="created_at"),
+    edited_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), comment="edited_at")
